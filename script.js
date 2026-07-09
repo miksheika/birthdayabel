76ed0495e0fcc187366a158bb0ff4403690e1b7b
miksheika <micksheika30@gmail.com>
2026-07-10 02:15:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// nanti diisi
+function openGift() {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+  startHeart();
+}
+
+function startHeart() {
+  setInterval(() => {
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+    heart.innerHTML = "❤";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (Math.random() * 20 + 18) + "px";
+    heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 5000);
+
+  }, 250);
+}
